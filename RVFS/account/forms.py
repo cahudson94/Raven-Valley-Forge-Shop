cfeb6f09366c22b9924112f9ecd8fd7ac87d092d
chud <c.ahudson84@yahoo.com>
2018-01-11 08:12:26 -0800

    Finished up some work of the home, about, and account.

--- RVFS/account/forms.py
+++ RVFS/account/forms.py
@@ -1,60 +1,60 @@
 """."""
-from registration.forms import RegistrationForm
 from django import forms
+from account.models import Account
 import datetime
 
 STATES = ([
-    ('ALABAMA', 'AL'),
-    ('ALASKA', 'AK'),
-    ('ARIZONA', 'AZ'),
-    ('ARKANSAS', 'AR'),
-    ('CALIFORNIA', 'CA'),
-    ('COLORADO', 'CO'),
-    ('CONNECTICUT', 'CT'),
-    ('DELAWARE', 'DE'),
-    ('FLORIDA', 'FL'),
-    ('GEORGIA', 'GA'),
-    ('HAWAII', 'HI'),
-    ('IDAHO', 'ID'),
-    ('ILLINOIS', 'IL'),
-    ('INDIANA', 'IN'),
-    ('IOWA', 'IA'),
-    ('KANSAS', 'KS'),
-    ('KENTUCKY', 'KY'),
-    ('LOUISIANA', 'LA'),
-    ('MAINE', 'ME'),
-    ('MARYLAND', 'MD'),
-    ('MASSACHUSETTS', 'MA'),
-    ('MICHIGAN', 'MI'),
-    ('MINNESOTA', 'MN'),
-    ('MISSISSIPPI', 'MS'),
-    ('MISSOURI', 'MO'),
-    ('MONTANA', 'MT'),
-    ('NEBRASKA', 'NE'),
-    ('NEVADA', 'NV'),
-    ('NEW HAMPSHIRE', 'NH'),
-    ('NEW JERSEY', 'NJ'),
-    ('NEW MEXICO', 'NM'),
-    ('NEW YORK', 'NY'),
-    ('NORTH CAROLINA', 'NC'),
-    ('NORTH DAKOTA', 'ND'),
-    ('OHIO', 'OH'),
-    ('OKLAHOMA', 'OK'),
-    ('OREGON', 'OR'),
-    ('PENNSYLVANIA', 'PA'),
-    ('RHODE ISLAND', 'RI'),
-    ('SOUTH CAROLINA', 'SC'),
-    ('SOUTH DAKOTA', 'SD'),
-    ('TENNESSEE', 'TN'),
-    ('TEXAS', 'TX'),
-    ('UTAH', 'UT'),
-    ('VERMONT', 'VT'),
-    ('VIRGINIA', 'VA'),
-    ('WASHINGTON', 'WA'),
-    ('WEST VIRGINIA', 'WV'),
-    ('WISCONSIN', 'WI'),
-    ('WYOMING', 'WY'),
-])
+    ('Alabama', 'AL'),
+    ('Alaska', 'AK'),
+    ('Arizona', 'AZ'),
+    ('Arkansas', 'AR'),
+    ('California', 'CA'),
+    ('Colorado', 'CO'),
+    ('Connecticut', 'CT'),
+    ('Delaware', 'DE'),
+    ('Florida', 'FL'),
+    ('Georgia', 'GA'),
+    ('Hawaii', 'HI'),
+    ('Idaho', 'ID'),
+    ('Illinois', 'IL'),
+    ('Indiana', 'IN'),
+    ('Iowa', 'IA'),
+    ('Kansas', 'KS'),
+    ('Kentucky', 'KY'),
+    ('Louisiana', 'LA'),
+    ('Maine', 'ME'),
+    ('Maryland', 'MD'),
+    ('Massachusetts', 'MA'),
+    ('Michigan', 'MI'),
+    ('Minnesota', 'MN'),
+    ('Mississippi', 'MS'),
+    ('Missouri', 'MO'),
+    ('Montana', 'MT'),
+    ('Nebraska', 'NE'),
+    ('Nevada', 'NV'),
+    ('New Hampshire', 'NH'),
+    ('New Jersey', 'NJ'),
+    ('New Mexico', 'NM'),
+    ('New York', 'NY'),
+    ('North Carolina', 'NC'),
+    ('North Dakota', 'ND'),
+    ('Ohio', 'OH'),
+    ('Oklahoma', 'OK'),
+    ('Oregon', 'OR'),
+    ('Pennsylvania', 'PA'),
+    ('Rhode Island', 'RI'),
+    ('South Carolina', 'SC'),
+    ('South Dakota', 'SD'),
+    ('Tennessee', 'TN'),
+    ('Texas', 'TX'),
+    ('Utah', 'UT'),
+    ('Vermont', 'VT'),
+    ('Virginia', 'VA'),
+    ('Washington', 'WA'),
+    ('West Virginia', 'WV'),
+    ('Wisconsin', 'WI'),
+    ('Wyoming', 'WY')]
+)
 
 current_year = datetime.datetime.now().year
 
@@ -62,14 +62,21 @@ YEARS = [x for x in range(current_year - 117, current_year - 17)]
 YEARS = YEARS[::-1]
 
 
-class CustomRegForm(RegistrationForm):
+class InfoRegForm(forms.ModelForm):
     """."""
 
-    first_name = forms.CharField(max_length=25)
-    last_name = forms.CharField(max_length=25)
+    user_name = forms.CharField(max_length=35)
+    # first_name = forms.CharField(max_length=25)
+    # last_name = forms.CharField(max_length=25)
     birth_date = forms.CharField(widget=forms.SelectDateWidget(years=YEARS))
     street = forms.CharField(max_length=90, label='Address Line 1')
     adr_extra = forms.CharField(required=False, max_length=90, label='Address Line 2')
     zip_code = forms.CharField(max_length=10, label='Zip Code')
     city = forms.CharField(max_length=25, label='City')
     state = forms.ChoiceField(required=True, choices=STATES, label='State')
+
+    class Meta():
+        """."""
+
+        model = Account
+        fields = ['first_name', 'last_name']
