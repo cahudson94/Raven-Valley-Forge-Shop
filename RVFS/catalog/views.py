cfd2535f8dcd2ddfcab97ed0d7d2579e189a3c44
chud <c.ahudson84@yahoo.com>
2018-01-15 01:15:54 -0800

    Catalog work mostly done, still need single views and finishing touches on base catalog.

--- RVFS/catalog/views.py
+++ RVFS/catalog/views.py
@@ -16,10 +16,34 @@ class CatalogView(ListView):
     def get_context_data(self, **kwargs):
         """Add context for active page."""
         context = super(ListView, self).get_context_data(**kwargs)
-        context['products'] = context['product_list'].filter(published='PB')
-        context['services'] = Service.objects.filter(published='PB')
-        context['prod_tags'] = sorted(set([tag for prod in context['products'] for tag in prod.tags.names()]))
-        context['serv_tags'] = sorted(set([tag for serv in context['services'] for tag in serv.tags.names()]))
+        products = Product.objects.filter(published='PB')
+        services = Service.objects.filter(published='PB')
+        context['prod_tags'] = sorted(set([tag for prod in products for tag in prod.tags.names()]))
+        context['serv_tags'] = sorted(set([tag for serv in services for tag in serv.tags.names()]))
+        context['all_tags'] = sorted(set(context['prod_tags'] + context['serv_tags']))
+        prod_paginator = Paginator(context['prod_tags'], 3)
+        serv_paginator = Paginator(context['serv_tags'], 3)
+        request = context['view'].request
+        if 'prod_page' in request.GET:
+            prod_page = request.GET.get('prod_page').split('?serv_page')[0]
+            serv_page = request.GET.get('prod_page').split('?serv_page')[1]
+        else:
+            prod_page = 1
+            serv_page = 1
+        try:
+            context['prod_tags'] = prod_paginator.page(prod_page)
+        except PageNotAnInteger:
+            context['prod_tags'] = prod_paginator.page(1)
+        except EmptyPage:
+            context['prod_tags'] = prod_paginator.page(prod_paginator.num_pages)
+        try:
+            context['serv_tags'] = serv_paginator.page(serv_page)
+        except PageNotAnInteger:
+            context['serv_tags'] = serv_paginator.page(1)
+        except EmptyPage:
+            context['serv_tags'] = serv_paginator.page(serv_paginator.num_pages)
+        context['products'] = {tag: [prod for prod in products if tag in prod.tags.names()] for tag in context['prod_tags']}
+        context['services'] = {tag: [serv for serv in services if tag in serv.tags.names()] for tag in context['serv_tags']}
         context['nbar'] = 'cata'
         return context
 
@@ -33,25 +57,33 @@ class AllProductsView(ListView):
     def get_context_data(self, **kwargs):
         """Add context for active page."""
         context = super(ListView, self).get_context_data(**kwargs)
-        products = context['product_list'].filter(published='PB')
-        context['tags'] = sorted(set([tag for prod in products for tag in prod.tags.names()]))
-        context['items'] = {tag: [prod for prod in products if tag in prod.tags.names()] for tag in context['tags']}
+        items = self.model.objects.filter(published='PB')
+        context['all_tags'] = sorted(set([tag for item in items for tag in item.tags.names()]))
+        paginator = Paginator(context['all_tags'], 5)
+        request = context['view'].request
+        if 'page' in request.GET:
+            page = context['view'].request.GET.get('page')
+        else:
+            page = 1
+        try:
+            context['tags'] = paginator.page(page)
+        except PageNotAnInteger:
+            context['tags'] = paginator.page(1)
+        except EmptyPage:
+            context['tags'] = paginator.page(paginator.num_pages)
+        context['items'] = {tag: [item for item in items if tag in item.tags.names()] for tag in context['tags']}
         context['nbar'] = 'prods'
         return context
 
 
-class AllServicesView(ListView):
+class AllServicesView(AllProductsView):
     """Catalogue view for the shop of all products and services."""
 
-    template_name = 'catalog.html'
     model = Service
 
     def get_context_data(self, **kwargs):
         """Add context for active page."""
-        context = super(ListView, self).get_context_data(**kwargs)
-        services = Service.objects.filter(published='PB')
-        context['tags'] = sorted(set([tag for serv in services for tag in serv.tags.names()]))
-        context['items'] = {tag: [serv for serv in services if tag in serv.tags.names()] for tag in context['tags']}
+        context = super(AllProductsView, self).get_context_data(**kwargs)
         context['nbar'] = 'servs'
         return context
 
@@ -62,19 +94,16 @@ class TagProductsView(ListView):
     template_name = 'tagged_catalog.html'
     model = Product
 
-    def get_queryset(self):
-        """Filter for the tag."""
-        return(Product.objects.filter(published='PB')
-                              .filter(tags__slug=[self.kwargs.get('slug')]).all())
-
     def get_context_data(self, **kwargs):
         """Add context for active page."""
         context = super(TagProductsView, self).get_context_data(**kwargs)
-        tag = self.kwargs.get('slug')
-        context['tag'] = tag
-        products = (Product.objects.filter(published='PB')
+        page_tag = self.kwargs.get('slug')
+        context['tag'] = page_tag
+        all_items = self.model.objects.filter(published='PB')
+        context['all_tags'] = sorted(set([tag for item in all_items for tag in item.tags.names()]))
+        items = (self.model.objects.filter(published='PB')
                                    .filter(tags__name__in=[self.kwargs.get('slug')]).all())
-        paginator = Paginator(products, 20)
+        paginator = Paginator(items, 20)
         page = context['view'].request.GET.get('page')
         try:
             context['items'] = paginator.page(page)
@@ -83,30 +112,19 @@ class TagProductsView(ListView):
         except EmptyPage:
             context['items'] = paginator.page(paginator.num_pages)
         context['nbar'] = 'prods'
-        context['tags'] = sorted(set([tag for prod in context['items'] for tag in prod.tags.names()]))
+        context['tags'] = sorted(set([tag for item in context['items'] for tag in item.tags.names()]))
         return context
 
 
-class TagServicesView(ListView):
+class TagServicesView(TagProductsView):
     """Catalogue view for the shop of all products and services."""
 
-    template_name = 'tagged_catalog.html'
     model = Service
 
     def get_context_data(self, **kwargs):
         """Add context for active page."""
-        context = super(ListView, self).get_context_data(**kwargs)
-        services = context['service_list'].filter(published='PB', tag__slug=kwargs['slug'])
-        paginator = Paginator(services, 20)
-        page = context['view'].request.GET.get('page')
-        try:
-            context['items'] = paginator.page(page)
-        except PageNotAnInteger:
-            context['items'] = paginator.page(1)
-        except EmptyPage:
-            context['items'] = paginator.page(paginator.num_pages)
+        context = super(TagProductsView, self).get_context_data(**kwargs)
         context['nbar'] = 'servs'
-        context['tags'] = sorted(set([tag for serv in context['items'] for tag in serv.tags.names()]))
         return context
 
 
@@ -120,6 +138,19 @@ class SingleProductView(DetailView):
         """Add context for active page."""
         context = super(DetailView, self).get_context_data(**kwargs)
         context['nbar'] = 'prods'
+        return context
+
+
+class SingleServiceView(DetailView):
+    """Detail view for a product."""
+
+    template_name = 'service.html'
+    model = Service
+
+    def get_context_data(self, **kwargs):
+        """Add context for active page."""
+        context = super(DetailView, self).get_context_data(**kwargs)
+        context['nbar'] = 'servs'
         return context
 
 
@@ -153,9 +184,6 @@ class EditProductView(PermissionRequiredMixin, UpdateView):
     template_name = 'edit_product.html'
     model = Product
     form_class = ProductForm
-
-    # def get_success_url(self):
-    #     """Redirect to detail page."""
 
 
 class CreateServiceView(PermissionRequiredMixin, CreateView):
